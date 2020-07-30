6528c74f5bd0a1249e779a05ae61ef88f38289f5
mrWotTakWot <68698093+mrWotTakWot@users.noreply.github.com>
2020-07-30 16:58:33 +0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.querySelector('#toggle').addEventListener("click", function(){
     document.querySelector('.header__nav-bar').classList.toggle('header__nav-bar__act')
 })
-
-document.querySelector('#toggle').addEventListener("click", function(){
-    this.classList.toggle('toggle__hidden')
-})
