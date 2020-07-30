8c1fad6dc14dd79f7e16322e78d59a8c2f90ca7d
mrWotTakWot <68698093+mrWotTakWot@users.noreply.github.com>
2020-07-30 17:08:29 +0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.querySelector('#toggle').addEventListener("click", function(){
     document.querySelector('.header__nav-bar').classList.toggle('header__nav-bar__act')
 })
+document.querySelector('#toggle').addEventListener("click", function(){
+    document.querySelector('.mn').classList.toggle('mn_h')
+})
