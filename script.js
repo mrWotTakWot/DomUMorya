cc9ccdee982f908f3b2ff3534724a6852a72bf9d
mrWotTakWot <68698093+mrWotTakWot@users.noreply.github.com>
2020-08-05 19:15:31 +0300

    5.08.2020

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector('#toggle').addEventListener("click", function(){
-    document.querySelector('.header__nav-bar').classList.toggle('header__nav-bar__act')
+    document.querySelector('.header__nav-bar-mob').classList.toggle('header__nav-bar-mob-vis')
 })
 document.querySelector('#toggle').addEventListener("click", function(){
     document.querySelector('.mn').classList.toggle('mn_h')
